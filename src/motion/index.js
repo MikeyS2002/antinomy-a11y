aa465cc924474f136e1a70c98481a70ad515600f
MikeyS2002 <mikeschaper12@gmail.com>
2026-03-26 14:46:20 +0100

    feat: export new motion utilities

--- src/motion/index.js
+++ src/motion/index.js
@@ -1,5 +1,8 @@
+export { adaptMotion } from "./adaptMotion.js";
 export { classifyProperty } from "./classify.js";
+export { isSafeEasing, isUnsafeTransitionType } from "./easing.js";
 export {
+    configureMotion,
     thresholds,
     maxDuration,
     reducedEasing,
